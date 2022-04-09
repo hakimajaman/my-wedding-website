e2ee5ae490fb57ebe4d2963868ad195649329561
hakimajaman <rifqitaufiqulhakim199922@gmail.com>
2022-04-09 17:06:34 +0700

    initiate mvc and tailwind

---
+++ app/react/pages/homepage/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../../assets/styles/index.css";
+
+const HomePage = () => {
+  return <div>homepage</div>;
+};
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/**/*.{html,js,ejs}",
+    "./app/assets/dist/*.{js}",
+    "./app/views/**/*.{js, ejs}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
